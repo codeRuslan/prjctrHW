7d7433ee0293638c8c65305ba521c07c1e22867d
Ruslan Pylypiuk <ruslan.pilipyuk@gmail.com>
2023-06-06 00:30:44 +0200

    HW1: Added func to catch animals into cages and small fixes

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Animal struct {
-	name         string
-	animalOwner  *Zookeper
-	cageAssigned *Cage
+	name           string
+	animalOwner    *Zookeper
+	caughtIntoCage bool
 }
 
 type Zookeper struct {
@@ -17,6 +20,7 @@ type Zookeper struct {
 type Cage struct {
 	cageSpeciality string
 	cageOwner      *Zookeper
+	caughtAnimal   []*Animal
 }
 
 func (a *Animal) GetName() string {
@@ -27,72 +31,89 @@ func (a *Animal) GetAnimalOwner() *Zookeper {
 	return a.animalOwner
 }
 
-func (a *Animal) GetAnimalCage() *Cage {
-	return a.cageAssigned
+func (a *Animal) GetTrueIfAnimalCage() bool {
+	return a.caughtIntoCage
 }
 
 func (a *Animal) GetDetailedSummaryOfAnimal() {
 	fmt.Printf("Detailed information about %s ---> ", a.GetName())
-	fmt.Println("Owner: "+a.GetAnimalOwner().firstName, ", Cage: "+a.GetAnimalCage().cageSpeciality)
+	fmt.Println("Owner: "+a.GetAnimalOwner().firstName, ", Cage: "+strconv.FormatBool(a.GetTrueIfAnimalCage()))
+}
+
+func (c *Cage) GetAnimalIntoCage(a *Animal) {
+	a.caughtIntoCage = true
+	c.caughtAnimal = append(c.caughtAnimal, a)
 }
 
 func main() {
 
-	MainZookeper := Zookeper{
+	mainZookeper := Zookeper{
 		firstName:  "Alex",
 		secondName: "Yurieev",
 	}
 
-	PredatorsCage := Cage{
+	predatorsCage := Cage{
 		cageSpeciality: "Predators",
-		cageOwner:      &MainZookeper,
+		cageOwner:      &mainZookeper,
 	}
 
-	NonPredatorsCage := Cage{
+	nonPredatorsCage := Cage{
 		cageSpeciality: "Non Predators",
-		cageOwner:      &MainZookeper,
+		cageOwner:      &mainZookeper,
 	}
 
-	Lion := Animal{
-		name:         "Lion",
-		animalOwner:  &MainZookeper,
-		cageAssigned: &PredatorsCage,
+	lion := Animal{
+		name:           "Lion",
+		animalOwner:    &mainZookeper,
+		caughtIntoCage: false,
 	}
 
-	Zebra := Animal{
-		name:         "Zebra",
-		animalOwner:  &MainZookeper,
-		cageAssigned: &NonPredatorsCage,
+	zebra := Animal{
+		name:           "Zebra",
+		animalOwner:    &mainZookeper,
+		caughtIntoCage: false,
 	}
 
-	Snake := Animal{
-		name:         "Snake",
-		animalOwner:  &MainZookeper,
-		cageAssigned: &PredatorsCage,
+	snake := Animal{
+		name:           "Snake",
+		animalOwner:    &mainZookeper,
+		caughtIntoCage: false,
 	}
 
-	Tiger := Animal{
-		name:         "Tiger",
-		animalOwner:  &MainZookeper,
-		cageAssigned: &PredatorsCage,
+	tiger := Animal{
+		name:           "Tiger",
+		animalOwner:    &mainZookeper,
+		caughtIntoCage: false,
 	}
 
-	Elephant := Animal{
-		name:         "Elephant",
-		animalOwner:  &MainZookeper,
-		cageAssigned: &NonPredatorsCage,
+	elephant := Animal{
+		name:           "Elephant",
+		animalOwner:    &mainZookeper,
+		caughtIntoCage: false,
 	}
 
-	MainZookeper.cagesOwned = append(MainZookeper.cagesOwned, &PredatorsCage, &NonPredatorsCage)
+	mainZookeper.cagesOwned = append(mainZookeper.cagesOwned, &predatorsCage, &nonPredatorsCage)
+	fmt.Println("\nDetailed Summary about animals: ")
+	tiger.GetDetailedSummaryOfAnimal()
+	lion.GetDetailedSummaryOfAnimal()
+	zebra.GetDetailedSummaryOfAnimal()
+	snake.GetDetailedSummaryOfAnimal()
+	elephant.GetDetailedSummaryOfAnimal()
 
-	Tiger.GetDetailedSummaryOfAnimal()
-	Lion.GetDetailedSummaryOfAnimal()
-	Zebra.GetDetailedSummaryOfAnimal()
-	Snake.GetDetailedSummaryOfAnimal()
-	Elephant.GetDetailedSummaryOfAnimal()
-
-	for i, cage := range MainZookeper.cagesOwned {
+	fmt.Println("\nCages:")
+	for i, cage := range mainZookeper.cagesOwned {
 		fmt.Println(i+1, " Speciality of Cage: "+cage.cageSpeciality, ", Cage Owner's Name: "+cage.cageOwner.firstName)
+	}
+
+	fmt.Println("\nPrint out if there any Animals inside predators Cage: ")
+	fmt.Println(predatorsCage.caughtAnimal)
+	fmt.Println("Catching tiger and lion...")
+	predatorsCage.GetAnimalIntoCage(&lion)
+	predatorsCage.GetAnimalIntoCage(&tiger)
+	fmt.Println("\nPrint out if there any Animals inside predators Cage once more: ")
+	for i, animal := range predatorsCage.caughtAnimal {
+		fmt.Println(i+1, animal.name)
+		fmt.Println(i+1, animal.caughtIntoCage)
 	}
 
 }
