07a90c0f2746f140420d7c2c100f5044e7a2290a
Ruslan Pylypiuk <ruslan.pilipyuk@gmail.com>
2023-07-06 01:21:10 +0200

    HW9: Update Task 1

--- HW9/TASK1/main.go
+++ HW9/TASK1/main.go
@@ -16,36 +16,43 @@ func main() {
 	tasks := []Task{
 		Task{"30.06.2023", "Wash dishes"},
 		Task{"29.06.2023", "Do homework"},
-		Task{"28.06.2023", "Do cleaning"},
+		Task{"29.06.2023", "Do cleaning"},
 		Task{"27.06.2023", "Do Homework"},
 	}
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
 		GetAllTasks(w, r, tasks)
-	})
+	}).Methods("GET")
 	myRouter.HandleFunc("/task/{date}", func(w http.ResponseWriter, r *http.Request) {
 		GetSingleTask(w, r, tasks)
-	})
+	}).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request, tasks []Task) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tasks)
 }
 
 func GetSingleTask(w http.ResponseWriter, r *http.Request, tasks []Task) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	key := vars["date"]
+	var foundTasks []Task
 
 	for _, task := range tasks {
 		if task.Date == key {
-			json.NewEncoder(w).Encode(task)
-			return
+			foundTasks = append(foundTasks, task)
 		}
 	}
 
-	// Task not found
-	w.WriteHeader(http.StatusNotFound)
+	if len(foundTasks) == 0 {
+		// Task not found
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(foundTasks)
 }
