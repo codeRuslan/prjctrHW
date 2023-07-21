ef2068e5608595867cffc0c5a67c5c55792c47ae
Ruslan Pylypiuk <ruslan.pilipyuk@gmail.com>
2023-07-21 05:12:58 +0200

    HW13: Task 1.1

---
+++ HW13/main.go
@@ -0,0 +1,37 @@
+package awesomeProject3
+
+import (
+	"awesomeProject3/fileOrganizer"
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	createFoldersStrategy := &fileOrganizer.CreateFolderStrategy{}
+	deleteFilesStrategy := &fileOrganizer.DeleteFilesStrategy{}
+	deleteFilesWithUnderscoreStrategy := &fileOrganizer.RegexDecorator{
+		RegexPattern: regexp.MustCompile("_"),
+		Strategy:     deleteFilesStrategy,
+	}
+
+	createFolderWithNameStrategy := &fileOrganizer.RegexDecorator{
+		RegexPattern: regexp.MustCompile("folder\\d+"),
+		Strategy:     createFoldersStrategy,
+	}
+
+	strategies := []fileOrganizer.FileOperationStrategy{
+		createFoldersStrategy,
+		deleteFilesStrategy,
+		deleteFilesWithUnderscoreStrategy,
+		createFolderWithNameStrategy,
+	}
+
+	for _, strategy := range strategies {
+		// Replace "actionPath" and "pattern" with your desired values
+		actionPath := "/path/to/your/target/directory"
+		err := strategy.Execute(actionPath)
+		if err != nil {
+			fmt.Println("Error executing strategy:", err)
+		}
+	}
+}
